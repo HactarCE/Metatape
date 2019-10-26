5561ba94356cd2db4e817a0b4c2161e993608c9f
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-10-26 15:10:32 -0400

    Add parsing demo in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+#[macro_use]
+extern crate pest_derive;
+
 mod metatape;
 
 fn main() {
     println!("Hello, world!");
+    println!(
+        "{:#?}",
+        metatape::parser::parse(
+            "
+ex<
+!h !e !l !l !o !_ !w !o !r !l !d !!
+
+
+@ _ { oo>o<ooooo }
+@ ! { oo>o<oooo>o< }
+@ d { o>oo<oo>o<oo }
+@ e { o>oo<oo>o<o>o< }
+@ h { o>o<oo>o<ooo }
+@ l { o>oo<o>oo<oo }
+@ o { o>oo<o>oooo< }
+@ r { o>ooo<oo>o<o }
+@ w { o>ooo<o>ooo< }
+            "
+        )
+    );
 }
