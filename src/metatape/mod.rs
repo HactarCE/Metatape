04e1be9e8c7503a3b14fd010e2dd096c196f9f74
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-10-27 01:38:58 -0400

    Create wrappers for producing a metatape::runtime::Runtime

--- src/metatape/mod.rs
+++ src/metatape/mod.rs
@@ -1,4 +1,15 @@
-pub mod parser;
-pub mod program;
-pub mod runtime;
+mod parser;
+mod program;
+mod runtime;
 mod tape;
+
+pub type Runtime<'a> = runtime::Runtime<'a>;
+pub type Program<'a> = program::Program<'a>;
+
+pub fn compile<'a>(program_source: &'a str) -> Result<Program<'a>, String> {
+    parser::parse(program_source).or_else(|parse_error| Err(parse_error.to_string()))
+}
+
+pub fn make_runtime<'a>(program: &'a Program, verbose: bool) -> Runtime<'a> {
+    Runtime::new(program, verbose)
+}
