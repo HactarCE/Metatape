f893dedabd0493d02da648c868027aff3c6209be
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-10-26 23:59:35 -0400

    Remove old testing code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,6 @@ extern crate pest_derive;
 mod metatape;
 
 fn main() -> Result<(), crate::metatape::parser::ParseError> {
-    //     println!("Hello, world!");
-
-    //     println!(
-    //         "{:#?}",
-    //         metatape::parser::parse(
-    //             "
-    // ex<
-    // !h !e !l !l !o !_ !w !o !r !l !d !!
-    // @ _ { oo>o<ooooo }
-    // @ ! { oo>o<oooo>o< }
-    // @ d { o>oo<oo>o<oo }
-    // @ e { o>oo<oo>o<o>o< }
-    // @ h { o>o<oo>o<ooo }
-    // @ l { o>oo<o>oo<oo }
-    // @ o { o>oo<o>oooo< }
-    // @ r { o>ooo<oo>o<o }
-    // @ w { o>ooo<o>ooo< }
-    // "
-    //         )
-    //     );
-
-    let program = metatape::parser::parse(
-        "
-[
-ex>e
-ex<<<<<<<<
-[eexix>(|])
-[<(])
-[>eo(xx<n>e|x)]
-xn<(])
-",
-    )?;
-
-    let instructions: Vec<(usize, &metatape::program::Instruction)> = program
-        .instructions
-        .iter()
-        .map(|(_, instruction)| instruction)
-        .enumerate()
-        .collect();
-
-    println!("{:#?}", instructions);
+    // while let Ok(()) = runtime.step() {}
     Ok(())
 }
